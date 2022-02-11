a68b7a9f3739f5d14b050e1dce2357a74d747c81
Michal Jan Warecki <mwarecki1@gmail.com>
2022-02-11 18:33:05 +0100

    Basic models

---
+++ models.py
@@ -0,0 +1,62 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+
+
+class Profile(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+class BankAccount(models.Model):
+    name = models.CharField(max_length=200)
+    number = models.BigIntegerField()
+    owner = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    balance = models.BigIntegerField()
+    # currency = models.CharField(max_length=3)
+
+
+class SpendingAccount(models.Model):
+    name = models.CharField(max_length=200)
+    owner = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    # currency = models.CharField(max_length=3)
+
+
+# class Category(models.Model):
+#     ...
+
+
+class Payee(models.Model):
+    name = models.CharField(max_length=200)
+    # owner = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    # default_category = models.ForeignKey(
+    #     Category, on_delete=models.SET_NULL, null=True, blank=True
+    # )
+
+
+class Receipt(models.Model):
+    from_account = models.ForeignKey(
+        BankAccount, on_delete=models.SET_NULL, null=True, blank=True
+    )
+    to_account = models.ForeignKey(
+        SpendingAccount, on_delete=models.SET_NULL, null=True, blank=True
+    )
+    payee = models.ForeignKey(Payee, on_delete=models.CASCADE)
+    # category = models.ForeignKey(
+    #     Category, on_delete=models.SET_NULL, null=True, blank=True
+    # )
+    date = models.DateTimeField()
+    amount = models.BigIntegerField()
+    # currency = models.CharField(max_length=3)
+
+
+class Transaction(models.Model):
+    from_account = models.ForeignKey(
+        BankAccount, on_delete=models.SET_NULL, null=True, blank=True, related_name="transactions_from"
+    )
+    to_account = models.ForeignKey(
+        BankAccount, on_delete=models.SET_NULL, null=True, blank=True, related_name="transactions_to"
+    )
+    date = models.DateTimeField()
+    amount = models.BigIntegerField()
+
