3fd2730a6dbb612ebb77b72005e93e546fee2ddb
Michal Jan Warecki <mwarecki1@gmail.com>
2022-02-11 19:26:50 +0100

    list view

--- views.py
+++ views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from django.views import View
 from .forms import ReceiptForm
+from .models import Receipt
+from datetime import datetime
 
 
 # Create your views here.
@@ -22,4 +24,13 @@ class add_receipt(View):
 
 class list(View):
     def get(self, request):
-        ...
+        context = {}
+        if 'day' in request.GET.keys():
+            context['day'] = datetime.fromisoformat(request['day'])
+        else:
+            context['day'] = datetime.now()
+        weekdate = context['day'].isocalendar()
+        context['days'] = [datetime.fromisocalendar(weekdate[0], weekdate[1], i) for i in range(1, 8)]
+        context['receipts'] = Receipt.objects.filter(date__gte=context['day'], date__lte=context['day'])
+
+        return render(request, "list.html", context=context)
