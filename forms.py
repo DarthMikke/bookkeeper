60421fd8ea856ae49661a6d2543168d8c1d11bc0
Michal Jan Warecki <mwarecki1@gmail.com>
2022-02-12 14:40:33 +0100

    PayeeForm

--- forms.py
+++ forms.py
@@ -1,8 +1,14 @@
 from django import forms
-from .models import Receipt
+from .models import Receipt, Payee
 
 
 class ReceiptForm(forms.ModelForm):
     class Meta:
         model = Receipt
+        fields = '__all__'
+
+
+class PayeeForm(forms.ModelForm):
+    class Meta:
+        model = Payee
         fields = '__all__'
