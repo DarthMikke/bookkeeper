788058b1d1f60d9bb1afc12e76766dec5a344267
Michal Jan Warecki <mwarecki1@gmail.com>
2022-02-13 17:34:10 +0100

    Transactions per payee monthly view.

--- tests.py
+++ tests.py
@@ -1,3 +1,47 @@
 from django.test import TestCase
+from .views import offset_month
+from datetime import datetime
 
-# Create your tests here.
+
+class TestMonthlyOffsets(TestCase):
+    def tests(self):
+        self.assertEqual(
+            offset_month(datetime(2021, 1, 1), -1),
+            datetime(2020, 12, 1)
+        )
+        self.assertEqual(
+            offset_month(datetime(2021, 1, 1), -2),
+            datetime(2020, 11, 1)
+        )
+        self.assertEqual(
+            offset_month(datetime(2021, 1, 1), -3),
+            datetime(2020, 10, 1)
+        )
+        self.assertEqual(
+            offset_month(datetime(2021, 1, 1), -4),
+            datetime(2020, 9, 1)
+        )
+        self.assertEqual(
+            offset_month(datetime(2021, 1, 1), -5),
+            datetime(2020, 8, 1)
+        )
+        self.assertEqual(
+            offset_month(datetime(2021, 12, 1), 1),
+            datetime(2022, 1, 1)
+        )
+        self.assertEqual(
+            offset_month(datetime(2021, 12, 1), 2),
+            datetime(2022, 2, 1)
+        )
+        self.assertEqual(
+            offset_month(datetime(2021, 12, 1), 3),
+            datetime(2022, 3, 1)
+        )
+        self.assertEqual(
+            offset_month(datetime(2021, 12, 1), 4),
+            datetime(2022, 4, 1)
+        )
+        self.assertEqual(
+            offset_month(datetime(2021, 12, 1), 5),
+            datetime(2022, 5, 1)
+        )
